0ddf83ff3499e3cb5a06c3a58dbdae04c5fb4a10
abdurraihan <abdurraihan898@gmail.com>
2023-05-20 00:03:52 +0600

    after rewrite code

--- controller/smtp_controller.js
+++ controller/smtp_controller.js
@@ -4,50 +4,49 @@ const nodemailer = require('nodemailer');
 exports.sendMail = async (req, res , next) => {
 
     try {
-        const to = req.body.to
-        const from = req.body.from;
-        const message = req.body.message;
-      
-const transporter =  nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: `${process.env.EMAIL}`,
-      pass: `${process.env.GMAIL_APP_PASS}`
-    }
-  });
+            const to = req.body.to
+            const from = req.body.from;
+            const message = req.body.message;
+                
+            const transporter =  nodemailer.createTransport({
+                service: 'gmail',
+                auth: {
+                user: `${process.env.EMAIL}`,
+                pass: `${process.env.GMAIL_APP_PASS}`
+                }
+            });
 
-  const mailOptions = {
-    from: `${from}`,
-    to: `${to}`,
-    subject: 'From Blood Line ',
-    text:` This mail if form ${from} see the actual  message bellow ....
+            const mailOptions = {
+                from: `${from}`,
+                to: `${to}`,
+                subject: 'From Blood Line ',
+                text:` This mail if form ${from} see the actual  message bellow ....
+                ${message}`
+            };
 
-    ${message}`
-  };
 
-
-  const email =  transporter.sendMail(mailOptions, function(error, info){
-    if (error) {
-      console.log(error);
-    } else {
-      console.log('Email sent: ' + info.response);
-    }
-  });
-        console.log(message);
-
-       
-      res.status(200).json({
-        status: 'success',
-        message: email,
-       
-      })
-  
-    } catch (error) {
-      res.status(400).json({
-        status: 'fail',
-        message: 'email not sand',
-        error: error.message
-      })
-    }
-  
+        const email =  transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+            console.log(error);
+            } else {
+            console.log('Email sent: ' + info.response);
+                
+            res.status(200).json({
+                status: 'success',
+                message: 'email send success full',
+            
+            })
+            }
+        });
+            
+    
+        
+            } catch (error) {
+            res.status(400).json({
+                status: 'fail',
+                message: 'email not sand',
+                error: error.message
+            })
+            }
+        
   }
