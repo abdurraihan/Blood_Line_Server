5db948ee9cbcf5ba3e2791b12ea88e10efb1ef86
abdurraihan <abdurraihan898@gmail.com>
2025-04-08 00:10:44 +0600

    build for deploy

--- controller/bloodSearch_controller.js
+++ controller/bloodSearch_controller.js
@@ -1,75 +1,64 @@
-const Donner = require('../model/donner.js');
+const Donner = require("../model/donner.js");
 
-exports.getAllDonner = async (req, res , next) => {
+exports.getAllDonner = async (req, res, next) => {
+  try {
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
 
-    try {  
+    const count = await Donner.find().countDocuments();
+    const result = await Donner.find().skip(skip).limit(limit);
 
-     const page = Number(req.query.page) || 1;
-     const limit = Number(req.query.limit) || 10;
-     const skip = (page - 1) * limit
-    
-     const count = await Donner.find().countDocuments();
-     const result = await Donner.find().skip(skip).limit(limit);
-    
-  
-      res.status(200).json({
-        status: 'success',
-        count : count , 
-        data: result
-      })
-  
-    } catch (error) {
-      res.status(400).json({
-        status: 'fail',
-        message: 'blood all searching fail',
-        error: error.message
-      })
-    }
-  
+    res.status(200).json({
+      status: "success",
+      count: count,
+      data: result,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      message: "blood all searching fail",
+      error: error.message,
+    });
   }
+};
 
+exports.getSingleBloodGroup = async (req, res, next) => {
+  try {
+    const group = req.query.group;
 
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
 
-exports.getSingleBloodGroup = async (req, res , next) => {
+    // For_plus_sign == %2B
+    // example For O+ == O%2B
+    // example For O- == O-
 
-    try {  
-    
-     const group = req.query.group;
-    
-     const page = Number(req.query.page) || 1;
-     const limit = Number(req.query.limit) || 10;
-     const skip = (page - 1) * limit
-    
+    const count = await Donner.find({
+      bloodGroup: `${group}`,
+    }).countDocuments();
+    const result = await Donner.find({ bloodGroup: `${group}` })
+      .skip(skip)
+      .limit(limit);
 
-// For_plus_sign == %2B 
-// example For O+ == O%2B 
-// example For O- == O- 
-
-const count = await Donner.find({bloodGroup:`${group}`}).countDocuments();
-const result =  await Donner.find({bloodGroup:`${group}`}).skip(skip).limit(limit);
-
-      if(result){
-          
+    if (result) {
       res.status(200).json({
-        status: 'success',
+        status: "success",
         count: count,
-        data: result
-      })
-
-      }else{
-        res.status(400).json({
-          status: 'fail',
-          data: "there is no bloodGroup"
-        })
-      }
-  
-    } catch (error) {
+        data: result,
+      });
+    } else {
       res.status(400).json({
-        status: 'fail',
-        message: 'blood searching fail',
-        error: error.message
-      })
+        status: "fail",
+        data: "there is no bloodGroup",
+      });
     }
-  
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      message: "blood searching fail",
+      error: error.message,
+    });
   }
-
+};
--- server.js
+++ server.js
@@ -1,27 +1,26 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const colors = require("colors");
 
 const app = require("./app");
 
-const connect = async () =>{
+const connect = async () => {
   try {
-      await mongoose.connect(process.env.MONGODB_CONNECTION_STRING);
-          console.log("connected to mongoDB".green.bold);
-    } catch (error) {
-      throw error;
-    }
+    console.log(process.env.MONGODB_CONNECTION_STRING);
+    await mongoose.connect(process.env.MONGODB_CONNECTION_STRING);
+    console.log("connected to mongoDB".green.bold);
+  } catch (error) {
+    throw error;
+  }
 };
 
-mongoose.connection.on('disconnected', () => {
+mongoose.connection.on("disconnected", () => {
   console.log(`mongodb disconnected`.red.bold);
 });
-
-
 
 const port = process.env.PORT || 50000;
 
 app.listen(port, () => {
-  connect(); 
+  connect();
   console.log(`App is running on port ${port}`.yellow.bold);
-}); 
+});
